5be2a5a9441f278213ee886c53917020c5268884
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2017-10-02 22:54:25 +0300

    Fix Derp

--- product/qcom-gps.mk
+++ product/qcom-gps.mk
@@ -1,2 +1,14 @@
+PRODUCT_PACKAGES += \
+    gps.msm8992 \
+    libcurl
+
+PRODUCT_PACKAGES += \
+    flp.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf \
+    xtwifi.conf
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml
